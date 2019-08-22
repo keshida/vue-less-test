4a2534b1a227aa15f9d08698192a7bdf56b1f213
张文旭 <zhangwx@topboya.com>
2019-08-22 16:54:45 +0800

    更新账单信息 添加js原型测试页面

---
+++ src/views/jsScript/prototypejs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pagePosition prototypejs_C">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {
+    function Person () {
+      this.a = 1;
+      this.b = 2;
+    }
+    let person1 = new Person();
+
+    console.clear();
+    Person.prototype.d=5
+    console.log(person1)
+    console.log(typeof Person)
+  },
+  mounted () {},
+  methods: {}
+}
+</script>
+<style>
+</style>
