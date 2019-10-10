8a66f4f224127df626cb8dd44c0a3082f5a8477f
张文旭 <zhangwx@topboya.com>
2019-10-10 17:42:13 +0800

    季度考核文件

--- src/views/jsScript/vueDrag.vue
+++ src/views/jsScript/vueDrag.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="pagePosition cueDrag_C">
-    <div class="demo">
+    <div class="myDrag" @touchstart="sadf" @touchmove="sadf" @touchend="sadfs">
+      sadfas
+    </div>
+    <!-- <div class="demo">
       <div class="drag"  v-drag></div>
     </div>
     <div class="demo2" v-drag:dragable>
         <div id="dragable"><span>这里可以拖动</span></div>
         <div class="content"><span>这里不可以</span></div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -17,16 +20,32 @@ export default {
   },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    sadf (e) {
+      console.log(e)
+    },
+    sadfs (e) {
+      console.log(e)
+      console.log(2)
+    }
+  }
 }
 </script>
 <style>
 .drag {
   position: absolute;
-  top: 20px;
-  left: 20px;
+  top: 100px;
+  left: 100px;
   width: 200px;
   height: 200px;
   background: green;
+}
+.myDrag {
+  height: 50px;
+  width: 50px;
+  background: red;
+  position: absolute;
+  top: 200px;
+  left: 200px;
 }
 </style>
