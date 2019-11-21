ea82683951fb4b717f1e1b7eb8aa7b478acd062e
张文旭 <zhangwx@topboya.com>
2019-11-21 17:17:39 +0800

    更新账单信息

--- src/views/brawser/Navigator.vue
+++ src/views/brawser/Navigator.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="navigator_C pagePosition">
+    <div>appCodeName: {{ navigatorObj.appCodeName }}</div>
+    <div>appName: {{ navigatorObj.appName }}</div>
+    <div>appVersion: {{ navigatorObj.appVersion }}</div>
+    <div>language: {{ navigatorObj.language }}</div>
+    <div>platform: {{ navigatorObj.platform }}</div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      adsfa: '',
+      navigatorObj: {},
       list: []
     }
   },
   created () {
     console.log(navigator)
+    this.navigatorObj = navigator
   },
   mounted () {
   },
@@ -24,4 +30,12 @@ export default {
 </script>
 
 <style>
+.navigator_C {
+  padding: 1rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
 </style>
