2a149f2bd13ec287fa5bf48e611060233b1d8968
zwx <zhangwx@topboya.com>
2020-05-15 15:29:40 +0800

    更新账单信息

--- src/views/jsScript/jsScriptHome.vue
+++ src/views/jsScript/jsScriptHome.vue
@@ -38,6 +38,20 @@ export default {
   created () {},
   mounted () {},
   methods: {
+    createNonceStr: function () {
+      var str = '',
+        range = 20,//min
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+ 
+      // 随机产生
+      range = Math.round(Math.random() * (36 - 20)) + 20;
+      for (let i = 0; i < range; i++) {
+        let pos = Math.round(Math.random() * (arr.length - 1));
+        
+        str += arr[pos];
+      }
+      return str;
+    },
     viewTheCom (index) {
       this.indexed = index;
     },
